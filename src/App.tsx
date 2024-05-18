bcccc0dad78f3fb82f6642e599816706484f7a3b
mykytos <kytyzovns@gmail.com>
2024-05-18 12:52:39 +0300

    created dynamic headline

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,15 @@ import './App.css';
 import {GlobalStyles, StyledBtn, StyledDiv, StyledImg} from "./Styles";
 import landscape from './images/landscape.jpg';
 import {themes} from "./myTheme";
-
+import StyledCard from "./Card";
 
 
 function App() {
   return (
     <div>
     <GlobalStyles/>
-      <StyledDiv>
-        <StyledImg src={landscape} alt="Landscape" />
-
-        <div className="div1">
-          <h2>Headline</h2>
-          <p>By mastering these properties,
-            you can add depth and dimension to your web elements,
-            enhancing the visual appeal of your designs.
-          </p>
-          <div className="div2">
-            <StyledBtn backColor = {themes.colors.blu} textColor={"white"}>See more</StyledBtn>
-            <StyledBtn textColor={themes.colors.blu}>Save</StyledBtn>
-          </div>
-        </div>
-
-      </StyledDiv>
+      <StyledCard headline={"First headline"}/>
+      <StyledCard headline={"Second headline"}/>
     </div>
   );
 }
---
+++ src/Card.tsx
@@ -0,0 +1,32 @@
+import {StyledBtn, StyledDiv, StyledImg} from "./Styles";
+import landscape from "./images/landscape.jpg";
+import {themes} from "./myTheme";
+import React from "react";
+
+type CardProps = {
+    headline: string;
+}
+
+function StyledCard(props: CardProps) {
+    return (
+
+        <StyledDiv>
+            <StyledImg src={landscape} alt="Landscape"/>
+
+            <div className="div1">
+                <h2>{props.headline}</h2>
+                <p>By mastering these properties,
+                    you can add depth and dimension to your web elements,
+                    enhancing the visual appeal of your designs.
+                </p>
+                <div className="div2">
+                    <StyledBtn backColor={themes.colors.blu} textColor={"white"}>See more</StyledBtn>
+                    <StyledBtn textColor={themes.colors.blu}>Save</StyledBtn>
+                </div>
+            </div>
+        </StyledDiv>
+    );
+}
+
+export default StyledCard;
+
